844b5c25370d036fe3edd175c9939b496f2c39b2
Greg Dubicki <grzegorz.dubicki@gmail.com>
2020-07-19 11:53:14 +0200

    Simplify timezone format - just the name

--- timestamp.go
+++ timestamp.go
@@ -6,7 +6,7 @@ import (
 )
 
 func timestamp() Row {
-	timestamp := time.Now().Format("2006-01-02 15:04:05 -07:00 MST")
+	timestamp := time.Now().Format("2006-01-02 15:04:05 MST")
 	return Row{
 		singleColorLabel("Information as of"),
 		singleColorValue(fmt.Sprintf("%s", timestamp)),
