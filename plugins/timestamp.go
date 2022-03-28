839d0f9d3c3409a16fe9e717908bbe1283977a59
Greg Dubicki <grzegorz.dubicki@gmail.com>
2022-03-28 21:24:18 +0100

    Revert "Make it obvious that it's YYYY-MM-DD"
    as it actually breaks the format. So long easter egg. :(
    
    This reverts commit 82966405dbfc1d3b4fae514e03b253abb3891002.

--- plugins/timestamp.go
+++ plugins/timestamp.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Timestamp() Row {
-	timestamp := time.Now().Format("1982-11-19 15:04:05 MST")
+	timestamp := time.Now().Format("2006-01-02 15:04:05 MST")
 	return Row{
 		Label: SingleColorLabel("Information as of"),
 		Value: SingleColorValue(fmt.Sprintf("%s", timestamp)),
